39004d45664085882dc7d7731419efb551a094fb
Robert Gerald Porter <rob@cartanova.ca>
2012-01-25 15:12:44 -0500

    Fix for associative array

--- assets/js/wmx.ini.js
+++ assets/js/wmx.ini.js
@@ -1,15 +1,13 @@
 var wmx = wmx || {};
 
 // define localized text strings
-wmx.localText = {
+wmx.localText = []
 
-	WEEVERMAPS_ERROR_NO_RESULTS: 'WEEVERMAPS_ERROR_NO_RESULTS',
-	WEEVERMAPS_CONFIRM_CLOSE: 'WEEVERMAPS_CONFIRM_CLOSE'
-	
-}
+wmx.localText["WEEVERMAPS_ERROR_NO_RESULTS"] 			=	'WEEVERMAPS_ERROR_NO_RESULTS';
+wmx.localText["WEEVERMAPS_CONFIRM_CLOSE"] 				=	'WEEVERMAPS_CONFIRM_CLOSE';
 
 // set the localization object
-wmx._txt = function(text) {	Joomla.JText._( wmx.localText(text) ); }
+wmx._txt = function(text) {	Joomla.JText._( wmx.localText[text] ); }
 
 // input fields for form
 wmx.inputField = {
